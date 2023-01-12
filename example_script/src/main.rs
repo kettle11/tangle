dd2717541c397b31d9cf4e7d758f4f70a4a94c42
Ian Kettlewell <ian.kettlewell@gmail.com>
2023-01-12 11:40:58 -0500

    Fix reentrancy issue

--- example_script/src/main.rs
+++ example_script/src/main.rs
@@ -45,6 +45,31 @@ extern "C" fn step() {
 }
 
 #[no_mangle]
+extern "C" fn fixed_update(time: u32) {
+    unsafe {
+        // let offset = (STATE.rects.len() * 10) as u32;
+
+        let time_frame = 6000; // 6 seconds
+        let t = (time % time_frame) as f32;
+        STATE.rects.clear();
+        STATE.rects.push((
+            Color {
+                r: (((t / time_frame as f32 * 3.14 * 2.0).sin() + 1.0) / 2.0 * 255.0) as u8,
+                g: 0,
+                b: 0,
+                a: 255,
+            },
+            Rectangle {
+                x: 100,
+                y: 100,
+                w: 100,
+                h: 100,
+            },
+        ));
+    }
+}
+
+#[no_mangle]
 extern "C" fn draw() {
     unsafe {
         for (Color { r, g, b, a }, Rectangle { x, y, w, h }) in STATE.rects.iter().copied() {
