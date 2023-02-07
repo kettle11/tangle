6b69a6a46f9b0903df7ffa70957db3b499a89a6a
Ian Kettlewell <ian.kettlewell@gmail.com>
2023-02-07 14:25:25 -0500

    Fixup test bed

--- build_web_example.sh
+++ build_web_example.sh
@@ -7,7 +7,7 @@ set -e
 # build the Rust
 cargo build --target wasm32-unknown-unknown --release --manifest-path rust_utilities/Cargo.toml
 cp rust_utilities/target/wasm32-unknown-unknown/release/rust_utilities.wasm tangle_ts/dist/rust_utilities.wasm
-
 cp rust_utilities/target/wasm32-unknown-unknown/release/rust_utilities.wasm web_example/rust_utilities.wasm
-
+cp tangle_ts/dist/index.js web_example/tangle.js
+cp tangle_ts/dist/index.js.map web_example/tangle.js.map
 
